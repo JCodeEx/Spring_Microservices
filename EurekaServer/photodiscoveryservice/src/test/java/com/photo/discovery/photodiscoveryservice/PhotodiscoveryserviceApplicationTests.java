975780b51aa59d009faadf2469a070e56563ca23
rasmin <rasminranjangiri8@gmail.com>
2025-09-09 12:18:29 +0530

    Added UserMicroservice and EurekaServer projects

---
+++ EurekaServer/photodiscoveryservice/src/test/java/com/photo/discovery/photodiscoveryservice/PhotodiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo.discovery.photodiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotodiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
